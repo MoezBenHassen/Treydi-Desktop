c7742a6816b718501c1d99cdd8533cd7ea0e3414
Moez Ben Hassen <37988307+MoezBenHassen@users.noreply.github.com>
2023-02-14 21:06:24 +0000

    utilisateur

--- src/Services/UtilisateurService.java
+++ src/Services/UtilisateurService.java
@@ -32,7 +32,9 @@ public class UtilisateurService implements IService<Utilisateur> {
         stm = con.createStatement();
         ResultSet result=stm.executeQuery(query);
         System.out.println(result.toString());
+
         List<Utilisateur> users = new ArrayList<>();
+
         while (result.next()){
             Utilisateur u = new Utilisateur(result.getInt("id_user"), result.getString("prenom"),
                     result.getString("nom"), result.getString("avatar_url"));
