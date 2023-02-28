589cf637384ab34d90d65c370865d8ccf196d242
Zeldariam <37988307+Zeldariam@users.noreply.github.com>
2023-02-28 01:05:27 +0100

    fx

--- src/javafxMain.java
+++ src/javafxMain.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -18,30 +21,36 @@ public class javafxMain extends Application {
 
         try {
             Parent root=FXMLLoader.load(getClass().getResource("/GUI/ListeArticles.fxml"));
-            Image image = new Image("GUI/Assets/images/log-04.png",32,32,true,true);
 
             Scene scene = new Scene(root);
 
-
             primaryStage.initStyle(StageStyle.UNDECORATED);
 
+            // Mouse drag scene
             scene.setOnMousePressed(event -> {
                 xOffset = event.getSceneX();
                 yOffset = event.getSceneY();
             });
-
-            scene.setOnMouseDragged(event -> {
-                primaryStage.setX(event.getScreenX() - xOffset);
-                primaryStage.setY(event.getScreenY() - yOffset);
+            scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    primaryStage.setX(event.getScreenX() - xOffset);
+                    primaryStage.setY(event.getScreenY() - yOffset);
+                }
             });
             // set window size
             primaryStage.setWidth(1600);
             primaryStage.setHeight(900);
-
+            // add css to scene
             scene.getStylesheets().add("GUI/Assets/css/style.css");
-
+            //Add app icon
+            Image image = new Image("GUI/Assets/images/log-04.png",32,32,true,true);
             primaryStage.getIcons().add(image);
+            //Add app title
             primaryStage.setTitle("Treydi ");
+            // make scene tranparent
+            scene.setFill(Color.TRANSPARENT);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
             primaryStage.setScene(scene);
             primaryStage.show();
 
